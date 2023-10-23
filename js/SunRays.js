c968980d9c84b257b4e1459c6328b8c2f0e3345a
Tessa Grainger <tessa.robyn@outlook.com>
2023-10-23 20:53:13 +0100

    Marginally better bouncing

--- js/SunRays.js
+++ js/SunRays.js
@@ -71,7 +71,7 @@ export class SunRay {
     if (num2 > Math.PI * 2) {
       num2 -= Math.PI * 2;
     }
-    return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+    return Math.floor(Math.random() * (num2 - num1)) + num1;
   }
   checkCollisionWithRing(ringX, ringY, ringRadius) {
     const distanceToRingCentre =
